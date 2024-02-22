89dccf14fbefa03f11cd1eeb5ed4a1d4ca91ab51
marko-panaiotov <108656266+marko-panaiotov@users.noreply.github.com>
2024-02-22 13:05:43 +0200

    Delete Java and DB practice H2/Java and DB practice directory

--- Java and DB practice H2/Java and DB practice/Ski Resort Project/src/Test.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Test {
-    private JPanel dfg;
-
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-    }
-}
